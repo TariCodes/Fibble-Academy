0c8f99b77af5cc8656d0916aed71891d8ac571fb
Godbless Ikadoi <godblessika@gmail.com>
2024-12-07 00:08:15 +0100

    add new changes to homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel/serverless';
 
-import netlify from '@astrojs/netlify';
+// import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  prefetch:{
+    prefetchAll: true
+  }
 });
